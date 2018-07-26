7aa0d144764941468a03076d8ff5e41e39d6b2aa
MaxZander <41649265+MaxZander@users.noreply.github.com>
2018-07-26 16:15:26 +0200

    Add files via upload

---
+++ Exo 6/MainApp.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Exo_6
+{
+    partial class MainApp
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_postal = new System.Windows.Forms.TextBox();
+            this.txt_OSIA = new System.Windows.Forms.TextBox();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.tex_lastname = new System.Windows.Forms.TextBox();
+            this.txt_adress = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_end = new System.Windows.Forms.Button();
+            this.btn_right = new System.Windows.Forms.Button();
+            this.btn_left = new System.Windows.Forms.Button();
+            this.btn_first = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_postal);
+            this.groupBox1.Controls.Add(this.txt_OSIA);
+            this.groupBox1.Controls.Add(this.txt_city);
+            this.groupBox1.Controls.Add(this.tex_lastname);
+            this.groupBox1.Controls.Add(this.txt_adress);
+            this.groupBox1.Controls.Add(this.txt_name);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(631, 261);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stagiaire";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Code Postal :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ville :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Adresse :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Prénom :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nom :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Numéro OSIA :";
+            // 
+            // txt_postal
+            // 
+            this.txt_postal.Location = new System.Drawing.Point(254, 188);
+            this.txt_postal.Name = "txt_postal";
+            this.txt_postal.Size = new System.Drawing.Size(257, 20);
+            this.txt_postal.TabIndex = 5;
+            // 
+            // txt_OSIA
+            // 
+            this.txt_OSIA.Location = new System.Drawing.Point(254, 58);
+            this.txt_OSIA.Name = "txt_OSIA";
+            this.txt_OSIA.Size = new System.Drawing.Size(257, 20);
+            this.txt_OSIA.TabIndex = 0;
+            // 
+            // txt_city
+            // 
+            this.txt_city.Location = new System.Drawing.Point(254, 162);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(360, 20);
+            this.txt_city.TabIndex = 4;
+            // 
+            // tex_lastname
+            // 
+            this.tex_lastname.Location = new System.Drawing.Point(254, 84);
+            this.tex_lastname.Name = "tex_lastname";
+            this.tex_lastname.Size = new System.Drawing.Size(360, 20);
+            this.tex_lastname.TabIndex = 1;
+            // 
+            // txt_adress
+            // 
+            this.txt_adress.Location = new System.Drawing.Point(254, 136);
+            this.txt_adress.Name = "txt_adress";
+            this.txt_adress.Size = new System.Drawing.Size(360, 20);
+            this.txt_adress.TabIndex = 3;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(254, 110);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(360, 20);
+            this.txt_name.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_end);
+            this.groupBox2.Controls.Add(this.btn_right);
+            this.groupBox2.Controls.Add(this.btn_left);
+            this.groupBox2.Controls.Add(this.btn_first);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox2.Location = new System.Drawing.Point(12, 279);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(631, 55);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Se déplacer";
+            // 
+            // btn_end
+            // 
+            this.btn_end.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_end.Location = new System.Drawing.Point(497, 19);
+            this.btn_end.Name = "btn_end";
+            this.btn_end.Size = new System.Drawing.Size(75, 23);
+            this.btn_end.TabIndex = 3;
+            this.btn_end.Text = ">> Dernier";
+            this.btn_end.UseVisualStyleBackColor = true;
+            // 
+            // btn_right
+            // 
+            this.btn_right.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_right.Location = new System.Drawing.Point(416, 19);
+            this.btn_right.Name = "btn_right";
+            this.btn_right.Size = new System.Drawing.Size(75, 23);
+            this.btn_right.TabIndex = 2;
+            this.btn_right.Text = "> Suivant";
+            this.btn_right.UseVisualStyleBackColor = true;
+            // 
+            // btn_left
+            // 
+            this.btn_left.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_left.Location = new System.Drawing.Point(335, 19);
+            this.btn_left.Name = "btn_left";
+            this.btn_left.Size = new System.Drawing.Size(75, 23);
+            this.btn_left.TabIndex = 1;
+            this.btn_left.Text = "< Précédent";
+            this.btn_left.UseVisualStyleBackColor = true;
+            // 
+            // btn_first
+            // 
+            this.btn_first.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_first.Location = new System.Drawing.Point(254, 19);
+            this.btn_first.Name = "btn_first";
+            this.btn_first.Size = new System.Drawing.Size(75, 23);
+            this.btn_first.TabIndex = 0;
+            this.btn_first.Text = "<< Premier";
+            this.btn_first.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            this.btn_add.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_add.Location = new System.Drawing.Point(428, 340);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Ajouter";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_exit.Location = new System.Drawing.Point(509, 340);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 3;
+            this.btn_exit.Text = "Quitter";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // MainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(654, 371);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainApp";
+            this.Text = "Affiche un Stagiaire";
+            this.Load += new System.EventHandler(this.MainApp_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_postal;
+        private System.Windows.Forms.TextBox txt_OSIA;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.TextBox tex_lastname;
+        private System.Windows.Forms.TextBox txt_adress;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_end;
+        private System.Windows.Forms.Button btn_right;
+        private System.Windows.Forms.Button btn_left;
+        private System.Windows.Forms.Button btn_first;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ Exo 6/MainApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exo_6
+{
+    public partial class MainApp : Form
+    {
+        AddUsers f1;
+        public MainApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            f1 = new AddUsers();
+
+            if (f1.ShowDialog(this) == DialogResult.OK)
+            {
+                this.txt_OSIA.Text = f1.txt_OSIA.Text;
+            }
+            else
+            {
+                MessageBox.Show("Ajout annulé.", "Annulation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            f1.Dispose();
+        }
+
+        private void f1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            f1 = null;
+        }
+
+        private void MainApp_Load(object sender, EventArgs e)
+        {
+            this.groupBox1.ForeColor = Color.White;
+            this.groupBox2.ForeColor = Color.White;
+            this.BackColor = Color.FromArgb(44, 62, 80);
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
